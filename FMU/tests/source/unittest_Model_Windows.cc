5ff46f30882a28210ca87cf426ffaa7fad77c88e
Jacob Chapman <jacoblchapman@gmail.com>
2016-03-13 16:55:45 +0100

    Changes to tests

--- FMU/tests/source/unittest_Model_Windows.cc
+++ FMU/tests/source/unittest_Model_Windows.cc
@@ -33,3 +33,36 @@ TEST_F(Test_Windows, Arrival) {
 
   mw.getDurationOpen();
 }
+
+
+TEST_F(Test_Windows, coefs) {
+
+  mw.setDurationVars(1, 1, 1);
+  mw.setArrivalVars(1, 1, 1, 1, 1);
+  mw.setInterVars(1, 1, 1, 1, 1);
+  mw.setDepartureVars(1, 1, 1, 1, 1, 1, 1, 1, 1);
+  mw.arrival(1, 1, 1, 1, 1);
+      EXPECT_EQ(1,mw.getAop());
+      EXPECT_EQ(1,mw.getBopout());
+      EXPECT_EQ(1,mw.getshapeop());
+      EXPECT_EQ(1,mw.getA01arr());
+      EXPECT_EQ(1,mw.getB01inarr());
+      EXPECT_EQ(1,mw.getB01outarr());
+      EXPECT_EQ(1,mw.getB01absprevarr());
+      EXPECT_EQ(1,mw.getB01rnarr());
+      EXPECT_EQ(1,mw.getA01int());
+      EXPECT_EQ(1,mw.getB01inint());
+      EXPECT_EQ(1,mw.getB01outint());
+      EXPECT_EQ(1,mw.getB01presint());
+      EXPECT_EQ(1,mw.getB01rnint());
+      EXPECT_EQ(1,mw.getA01dep());
+      EXPECT_EQ(1,mw.getB01outdep());
+      EXPECT_EQ(1,mw.getB01absdep());
+      EXPECT_EQ(1,mw.getB01gddep());
+      EXPECT_EQ(1,mw.getA10dep());
+      EXPECT_EQ(1,mw.getB10indep());
+      EXPECT_EQ(1,mw.getB10outdep());
+      EXPECT_EQ(1,mw.getB10absdep());
+      EXPECT_EQ(1,mw.getB10gddep());
+
+}
