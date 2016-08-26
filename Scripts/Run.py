55a42b54e89187cd2e8c75bae53632d9ecf47872
Jacob Chapman <Jacoblchapman@gmail.com>
2016-08-26 10:14:42 +0200

    Got script to set learning parameters

--- Scripts/Run.py
+++ Scripts/Run.py
@@ -11,8 +11,11 @@ nomass.configurationDirectory = home +"/git/No-MASSDSM/Configuration/WinterWeek/
 
 nomass.resultsLocation = "../FMU/build/Results/"
 nomass.PVFile = "PVBowler2013_365.csv"
+nomass.epsilon = 0.1
+nomass.alpha = 0.1
+nomass.gamma = 0.1
 nomass.printInput = True
-nomass.numberOfSimulations = 101
-nomass.learnUpToSimulation = 100
+nomass.numberOfSimulations = 10
+nomass.learnUpToSimulation = 5
 nomass.simulate()
 nomass.deleteLearningData()
