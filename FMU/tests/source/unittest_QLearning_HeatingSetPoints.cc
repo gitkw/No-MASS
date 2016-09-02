c25d927c260725ca4ea8c215bd92196f2accc186
Jacob Chapman <Jacoblchapman@gmail.com>
2016-09-02 10:47:22 +0200

    More test fixes

--- FMU/tests/source/unittest_QLearning_HeatingSetPoints.cc
+++ FMU/tests/source/unittest_QLearning_HeatingSetPoints.cc
@@ -38,6 +38,7 @@ TEST_F(Test_QLearning_HeatingSetPoints, learn) {
     zs.id = 1;
     Building_Zone z_Kitchen;
     z_Kitchen.setName(zs.name);
+    z_Kitchen.setActive(true);
     z_Kitchen.setup(zs);
 
     DataStore::addValue("Block1:Zone1ZoneMeanAirTemperature", 1);
